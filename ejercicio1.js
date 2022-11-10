3387aedfea556564bc1d3c7dbb0ef40e18d5483f
Rocio Lucía Junquera Melchor <113196108+RLuciaJunqueraBosonit@users.noreply.github.com>
2022-11-10 11:38:19 +0100

    Ejercicio1
    Subo ejercicio 1 del modulo de Javascript formación Frontend

---
+++ ejercicio1.js
@@ -0,0 +1,14 @@
+// Ejercicio 1: "Dado un array de objetos, obtener el objeto con el id 3"
+
+const arrNames = [
+  { id: 1, name: 'Pepe' },
+  { id: 2, name: 'Juan' },
+  { id: 3, name: 'Alba' },
+  { id: 4, name: 'Toby' },
+  { id: 5, name: 'Lala' }
+];
+
+
+const found = arrNames.find(element => element.id === 3);
+
+console.log(found);
